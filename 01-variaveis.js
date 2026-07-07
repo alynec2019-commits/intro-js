e2bbd328c723f8d64246a858f983e10ab59dac2b
alyne cristina <alynec2019@gmail.com>
2026-07-07 20:19:53 -0300

    aprendendo variaveis

--- 01-variaveis.js
+++ 01-variaveis.js
@@ -3,6 +3,25 @@
 
 // No javascript, temos 3 formas de declarar uma variável
 
-// 1 - const 
+// 1 - const - cria uma constante. Não pode receber outro valor
 const nome = "Mago"
-console.log(nome)
+console.log(nome) 
+
+// redeclarando uma variável. É o ato de trocar o seu valor
+//nome = "Alyne"
+
+// o javascript é case sensitive, ou seja, significa
+// 2 - let
+let NomeDaPessoa = "Alyne"
+
+// Redeclarando uma variavel do tipo let
+
+nomeDaPessoa = "Alyne"
+console.log(nomeDaPessoa)
+
+//3 - var
+var fruta = "Banana"
+console.log(fruta)
+
+// var é a forma antiga de se declarar variáveis no javascript.
+// ainda funciona, mas, normalmente evitamos usar em código por que o seu comportamento pode quebrar a aplicação.
