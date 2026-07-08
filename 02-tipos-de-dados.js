84e039ef0ab7bf8757763691e168adce0de6ecc1
alyne cristina <alynec2019@gmail.com>
2026-07-07 21:58:42 -0300

    Aprendendo os tipos de dados

---
+++ 02-tipos-de-dados.js
@@ -0,0 +1,44 @@
+// Os tipos de dados primitivos do javascript são:
+
+// 1 - string 
+// cadeia de caractéres, usamos para texto
+let nome = "Alyne"
+let sobrenome = "Cristina"
+
+console.log(nome)
+
+//2 - number
+// serve para armazenar numeros, sendo eles inteiros ou decimais
+let idade = 20
+let preco = 19.90
+
+// 3 - bigint
+// serve para garantir a precisão de numeros muito grandes
+let numero = 1549824682516498312056156721217612n
+
+// 4 - boolean
+//possui apenas dois valores sendo ele o false (falso) e true (verdadeiro)
+let conectado = false
+conectado = true
+
+let ligado = true
+let desligado = false
+
+// 5 - undefined - não defonido
+// significa que uma variável foi criado, mas não recebel valor
+let nomePessoa 
+console.log(nomePessoa)
+
+// 6 - null - vazio
+// representa um valor vazio definido intencionalmente
+let usuario = null  
+
+// 7 - Sybol - é um identificador unico
+// uso mais comum é em bibliotecas de códigos mais avançados
+const id1 = Symbol("@")
+const id2 = Symbol()
+
+console.log(id1)
+console.log(id2)
+
+// 8 - 
