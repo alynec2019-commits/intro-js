8daed09ee18537e5967c08ea4984979498a500cb
alyne cristina <alynec2019@gmail.com>
2026-07-13 21:49:48 -0300

    trabalhando com operadores aritimeticos

---
+++ 05-operadores-aritimeticos.js
@@ -0,0 +1,32 @@
+ let a = 20
+ let b = 10
+
+ // operadores aritiméticos são usados pra realizar cálculos matemáticos
+
+ // 1 - adição +
+ console.log(a + b)
+
+ //exemplo
+ console.log("1" + 1)
+
+ // 2 - subtração -
+ console.log(a - b)
+ console.log(20 - 10.45)
+
+ // 3 - multiplicação
+ console.log(a * b)
+ console.log(5.23 * 9.0)
+ console.log(-5 * -45)
+
+ // 4 - divisão /
+ console.log(a / b)
+ console.log(123 / 23.54)
+ console.log(-54 / 13.54)
+
+ // 5 - resto da divisão|operador de modulo
+ // muito usado para descobrir se o numero ´pe par ou impar
+ let numeroPar = 8
+ console.log(numeroPar % 2)
+
+  let numeroImpar = 7
+ console.log(numeroImpar % 2)
