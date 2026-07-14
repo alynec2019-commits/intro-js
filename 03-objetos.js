5f42a0b4de925d524dd6b3932fa1940c51b21578
alyne cristina <alynec2019@gmail.com>
2026-07-13 21:17:56 -0300

    aprendendo os operadores de comparação

---
+++ 03-objetos.js
@@ -0,0 +1,16 @@
+const pessoa = {
+    nome: "Alyne",
+    idade: 23,
+}
+
+
+const mochila = {
+    garrafa: "Água",
+    caderno: "Matemática",
+    notebook: "MacBook",
+}
+
+const nome = "Victor"
+console.log(mochila.caderno)
+console.log(mochila.garrafa)
+
